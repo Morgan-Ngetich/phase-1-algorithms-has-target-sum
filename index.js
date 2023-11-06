6ccea4a061f16174f177e735c1e49b2a24d33e83
Morgan-Ngetich <ngetichmorgan6@gmail.com>
2023-11-06 16:38:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === target ) {
+        return true
+      } 
+    }   
+  }
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n^2)
 */
 
 /* 
   Add your pseudocode here
+   check the array, 
+    if there is number that can be added to another in the array to equal target
+     return true
+    return false
 */
 
 /*
